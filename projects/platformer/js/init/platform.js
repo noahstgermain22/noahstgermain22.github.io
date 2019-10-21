018ee32ba2f10bcc5a2f092e78ed8f50fd97c654
noahstgermain5 <noahstgermain5@gmail.com>
2019-10-21 13:02:48 +0000

    create awesome platformer game

---
+++ projects/platformer/js/init/platform.js
@@ -0,0 +1,56 @@
+/*eslint-disable no-undef-expression, no-undef*/
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.platform = window.opspark.platform || {};
+    
+    let platform = window.opspark.platform;
+    
+   platform.create(400, 460);
+   platform.create(420, 440);
+   platform.create(440, 480);
+   collectable.create(type.max, 400, 460);
+   collectable.create(type.grace, 420, 440);
+   collectable.create(type.steve, 420, 440);
+   cannon.create.onTop(500);
+cannon.create.onLeft(300);
+cannon.create.onRight(400); 
+   /**
+     * init: This function initializes the platforms for the level.
+     * 
+     * GOAL: Add as many platforms necessary to make your level challenging.
+     * 
+     * Use the platform.create() method to create platforms for the level. 
+     * 
+     * platform.create() takes these arguments:
+     *      
+     *      platform.create(x, y, scaleX, scaleY);
+     * 
+     *      x: The x coordineate for the platform.
+     *      y: The y coordineate for the platform.
+     *      scaleX: OPTIONAL The scale factor on the x-axis, this value will 
+     *              stretch the platform in width.
+     *      scaleY: OPTIONAL The scale factor on the y-axis, this value will 
+     *              stretch the platform in height.
+     */ 
+    platform.init = function (game) {
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        /*
+         * ground : here, we create a floor. Given the width of of the platform 
+         * asset, giving it a scaleX and scaleY of 2 will stretch it across the 
+         * bottom of the game.
+         */
+        platform.create(0, game.world.height - 32, 3, 2);
+
+        // example:
+        platform.create(400, 460);
+        platform.create(500, 200);
+        
+        
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+})(window);
